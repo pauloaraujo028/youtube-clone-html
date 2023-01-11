4d959bf690b6706ee5b47fcc3a055bfda5cc8bda
Paulo Araújo <pauloaraujo.phz@gmail.com>
2023-01-11 12:03:19 -0300

    Primeiro commit

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const videoCardContainer = document.querySelector(".video-container");
 
-let api_key = "AIzaSyC_yDg2lBbuXBk3NkXicrtmNDZ1lgjwKCs";
+let api_key = "";
 let video_http = "https://www.googleapis.com/youtube/v3/videos?";
 let channel_http = "https://www.googleapis.com/youtube/v3/channels?";
 
